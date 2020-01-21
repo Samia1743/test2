a0e2e0dd7101469ce70b31ff1812e95c0721e4fb
Samia <34365874+Samia1743@users.noreply.github.com>
2020-01-21 20:44:16 +0600

    added extra prompt in line

--- 1.py
+++ 1.py
@@ -2,11 +2,11 @@ x=str(input())
 for i in range(5):
     print(x)
 
-
-
-
 hungry=input("Are you Hungry?")
 if hungry=="yes":
     print("eat somosa")
+    print("eat pasta")
 else:
     print("do your homework")
+
+
